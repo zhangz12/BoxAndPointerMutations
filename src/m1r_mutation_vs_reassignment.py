dd77f5dd62805d3e502a6d6faf5273eb75749431
zhangz12 <zhangz12@rose-hulman.edu>
2017-12-13 10:43:39 -0500

    m1

--- src/m1r_mutation_vs_reassignment.py
+++ src/m1r_mutation_vs_reassignment.py
@@ -4,8 +4,8 @@ This module demonstrates the distinction betweeen:
   -- RE-ASSIGNING the name BLAH to refer to a different object.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Zheming Zhang.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -17,12 +17,12 @@ import rosegraphics as rg
 
 def main():
     """ Runs several examples. """
-#     example1()
-#     example2()
+    example1()
+    example2()
 
 
 # ----------------------------------------------------------------------
-# TODO: 2.
+# DONE: 2.
 #  Step a:  READ the code in  example1.
 #
 #  Step b:  PREDICT what  example1  will print when it runs.
@@ -33,7 +33,7 @@ def main():
 #  Step d:  See whether your prediction matched the actual output.
 #           Ask questions as needed to resolve any differences.
 #
-#  Step e:  When you believe that you understand the code in example1,
+#  Step e:  When you believe thahttps://github.com/zhangz12/BoxAndPointerMutations.gitt you understand the code in example1,
 #           change the above TO DO to DONE.
 # ----------------------------------------------------------------------
 
@@ -61,7 +61,7 @@ def example1():
 
 
 # ----------------------------------------------------------------------
-# TODO: 3.
+# DONE: 3.
 #  Step a:  READ the code in  example2.
 #           Pay close attention to the difference between the functions:
 #              mutate_point
